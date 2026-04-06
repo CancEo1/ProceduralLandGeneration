265ef1ae33f559fbe12dae07a96cdac856a2e6f9
Cody Kern <codykern42@gmail.com>
2026-04-05 20:39:32 -0600

    Started Data storage

---
+++ Assets/Scripts/Data/NoiseData.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class NoiseData : ScriptableObject
+{
+    public Noise.NormalizeMode normalizeMode;
+
+    public float noiseScale;
+
+    public int octaves;
+    [Range(0, 1)]
+    public float persistance;
+    public float lacunarity;
+
+    public int seed;
+    public Vector2 offset;
+
+
+}
--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -13,22 +13,11 @@ public class MapGenerator : MonoBehaviour
     public enum DrawMode {NoiseMap, ColourMap, Mesh, FalloffMap};
     public DrawMode drawMode;
 
-    public Noise.NormalizeMode normalizeMode;
-
     public bool useFlatShading;
 
     [Range(0,6)]
     public int editorPreviewLOD;
-    public float noiseScale;
-
-    public int octaves;
-    [Range(0,1)]
-    public float persistance;
-    public float lacunarity;
-
-    public int seed;
-    public Vector2 offset;
-
+   
     public bool useFalloff;
 
     public float meshHeightMultiplier;
